1d0c539037e0f7fd59aa60ff8175d18876b9befc
vagusX <vagusxl@gmail.com>
2022-04-12 09:08:25 +0800

    feat: add highlight-js for code

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,11 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import { remark } from "remark";
+import html from "remark-html";
+
+import { rehype } from "rehype";
+import rehypeHighlight from "rehype-highlight";
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const vfile = await remark().use(html).process(markdown);
+  const result = await rehype().use(rehypeHighlight).process(vfile);
+  return result.toString();
 }
